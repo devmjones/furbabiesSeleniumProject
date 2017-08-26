65fcb9f6ee0eec307d80f71282ec77cef57143e3
Devon Jones <devonjones@Devons-MacBook-Pro.local>
2017-08-26 15:46:12 -0600

    UI mapping done for basic sign in and sign up fields

---
+++ src/main/java/SignUpUIMap.java
@@ -0,0 +1,11 @@
+/**
+ * Created by devonjones on 8/26/17.
+ */
+public class SignUpUIMap {
+    public static final String firstNameField = "input[name='Firstname']";
+    public static final String lastNameField = "input[name='Lastname']";
+    public static final String emailField = "#email";
+    public static final String passwordField = "#password";
+    public static final String submitBtn = "button[type='submit']";
+    public static final String signInLink = "//a[contains(text(), 'Sign In')]";
+}
---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by devonjones on 8/26/17.
+ */
+public class SignUpTest {
+}
